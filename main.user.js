ac6d70cf79b92e79fce409dc36a4f4f0271e8ecd
gongfuture <gongfuture@outlook.com>
2023-04-16 11:52:33 +0800

    初步完成修改

--- main.user.js
+++ main.user.js
@@ -8,19 +8,36 @@
 // @supportURL   https://github.com/KSroido/UJS_jiaoPing/issues/
 // @downloadURL  https://raw.githubusercontent.com/KSroido/UJS_jiaoPing/main/main.user.js
 // @updateURL    https://raw.githubusercontent.com/KSroido/UJS_jiaoPing/main/main.user.js
-// @match        https://*.ujs.edu.cn/*/xsjxpj2_2.aspx*
+// @match        https://*.ujs.edu.cn/http/*/xspjgl/*.html?*
 // @grant        none
 // ==/UserScript==
 
 function autoPj() {
     var rad_a,rad_b;
-    rad_a = "Datagrid1__ctl";
-    rad_b = "_rb_0";
-    for(var i=2;i<=11;i++)
+    rad_a = "pjf_F9066C26C99";
+    rad_b = "89D1E05383001CAC1890";
+    for(var i=0x4;i<=0xC;i++)
     {
-        document.getElementById(rad_a+i+rad_b).checked = true;
+        var m = i.toString()
+        m = m.toUpperCase()
+        document.getElementById(rad_a+m+rad_b).value = 100;
     }
     //document.getElementById("Datagrid1:__ctl8:rb").checked = true;
-    document.getElementById("txt_pjxx").value = '老师备课充分，授课重点突出。';
-    document.getElementById("Button1").click();
-} autoPj();
+    document.getElementById("py").value = '老师备课充分，授课重点突出。';
+    // document.getElementById("Button1").click();
+}
+autoPj();
+
+
+// https://webvpn.ujs.edu.cn/http/*/xspjgl/kcgcpj_cxKcgcpjxxIndex.html
+
+
+// pjf_F9066C26C99489D1E05383001CAC1890
+// pjf_F9066C26C99589D1E05383001CAC1890
+// pjf_F9066C26C99689D1E05383001CAC1890
+// pjf_F9066C26C99789D1E05383001CAC1890
+// pjf_F9066C26C99889D1E05383001CAC1890
+// pjf_F9066C26C99989D1E05383001CAC1890
+// pjf_F9066C26C99A89D1E05383001CAC1890
+// pjf_F9066C26C99B89D1E05383001CAC1890
+// pjf_F9066C26C99C89D1E05383001CAC1890
